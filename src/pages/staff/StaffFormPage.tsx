1d2e2444cfd14819b0bc6ce0115a5555dad579cb
temkin236 <temkin236@users.noreply.github.com>
2025-12-13 22:11:07 +0300

    final

--- src/pages/staff/StaffFormPage.tsx
+++ src/pages/staff/StaffFormPage.tsx
@@ -263,9 +263,9 @@ const StaffFormPage = () => {
           </div>
         </div>
       </div> {/* End Live Preview Section */}
-    </div>
-  );
-}
+      </div>
+    );
+  }
 
-export default StaffFormPage;
+  export default StaffFormPage;
 
